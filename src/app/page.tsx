38363dbdfe9418f14bb7949f3c666d0a95a301cf
Alex_Malv <a.malinovsky303@gmail.com>
2023-08-05 21:29:49 +0200

    fi style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="p-10">
       <h1 className="text-center text-xl font-extrabold">Peach</h1>
-      <div className="flex flex-col items-center gap-2 p-2 sm:flex-row sm:justify-between">
+      <div className="flex flex-col items-center gap-2  p-2 sm:flex-row sm:items-baseline sm:justify-between">
         <Card className="w-[20rem]">
           <CardHeader>
             <CardTitle>App</CardTitle>
